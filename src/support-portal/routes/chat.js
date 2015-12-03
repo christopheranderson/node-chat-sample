32e6c8bd50182be41963f96c3475ae35ce456a18
Christopher Anderson <chrande@microsoft.com>
2015-12-03 12:02:31 -0800

    Use WEBSITE_HOSTNAME instead of siteName env variables

--- src/support-portal/routes/chat.js
+++ src/support-portal/routes/chat.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET chat page. */
 router.get('/', function(req, res, next) {
-  res.render('chat', {siteName: (process.env['siteName'] || null)});
+  res.render('chat', {siteName: ((process.env['WEBSITE_HOSTNAME'] ? "http://" + process.env['WEBSITE_HOSTNAME'] : null)});
 });
 
 module.exports = router;
