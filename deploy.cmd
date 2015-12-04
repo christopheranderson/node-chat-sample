d4003a8dbd30513d702cd822dcb0e5387a77e195
Christopher Anderson <christopheranderson@Christophers-MacBook-Pro.local>
2015-12-04 12:49:44 -0800

    Reorder deploy steps

--- deploy.cmd
+++ deploy.cmd
@@ -99,29 +99,8 @@ IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
 :: 2. Select node version
 call :SelectNodeVersion
 
-:: 3. Install bower packages
-echo Checking Bower install
-IF EXIST "%DEPLOYMENT_TARGET%\bower.json" (
-  echo Starting Bower install
-  pushd "%DEPLOYMENT_TARGET%"
-    bower install
-    IF !ERRORLEVEL! NEQ 0 goto error
-  popd
-)
-
-:: 4. Install npm packages
-echo Checking NPM install
-IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
-  echo Starting NPM install
-  pushd "%DEPLOYMENT_TARGET%"
-  ::call :ExecuteCmd !NPM_CMD! install --production
-  npm install --production
-  echo Finished NPM install
-  IF !ERRORLEVEL! NEQ 0 goto error
-  echo No errors; continuing...
-  popd
-)
-
+npm install --production
+bower install
 echo Deploy completed!
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
